451992a79da1e6269506cd41ec4ab1c0c684278f
tyreln <tyrelnarciso@hotmail.com>
2022-01-10 09:56:04 -0800

    revised donate implementation

--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -5,19 +5,18 @@
   >
     <div class="q-mx-md q-mt-lg" style="border-radius: 25px">
       <div class="text-h3 text-center q-ma-sm">Donate</div>
-      <div class="text q-ma-xl">
+      <div class="text q-mx-xl q-my-md">
         If you would like to support our rescue work at NVARS, you may safely
         donate using one of the following methods:
       </div>
-      <div class="shadow-14 q-ma-md q-pa-xl">
+      <div class="shadow-14 q-mx-md q-pa-xl">
         <div class="text-h6 text-center">Interac E-Transfer:</div>
         <div class="q-my-lg text">
-          E-transfers will be available to be sent to donate@NVARS.com and are not subject to the
-          fees associated with the other methods, meaning we get all of it!
+          E-transfers sent to donate@NVARS.com are not subject to extra fees, meaning we get all of it!
         </div>
-        <div class="text-h6 q-mt-xl text-center">Credit Card, Paypal and PAD:</div>
-        <div class="flex" style="text-align: center; display:block;">
-        <iframe src="https://donorbox.org/embed/animal-rescue-donations" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%" style="max-width: 430px; min-width: 250px; max-height:none!important"></iframe>
+        <div class="text-h6 q-my-md text-center">Credit Card, Paypal and PAD:</div>
+        <div class="flex q-mb-md" style="text-align: center; display:block;">
+        <a style="background: #393946 url(https://donorbox.org/images/red_logo.png) no-repeat 37px;color: #fff;text-decoration: none;font-family: Verdana,sans-serif;display: inline-block;font-size: 16px;padding: 15px 38px;padding-left: 75px;-webkit-border-radius: 2px;-moz-border-radius: 2px;border-radius: 2px;box-shadow: 0 1px 0 0 #1f5a89;text-shadow: 0 1px rgba(0, 0, 0, 0.3);" href="https://donorbox.org/nvars-donate?default_interval=o&amount=30">Donate</a>
         </div>
   </div>
 
@@ -26,10 +25,6 @@
 </template>
 
 <script setup>
-import { useQuasar } from "quasar";
-import { api } from "../boot/axios";
-import { ref, onMounted, computed } from "vue";
-
 import { useMeta } from "quasar";
 useMeta({
   title: "Donate",
@@ -38,10 +33,4 @@ useMeta({
     description:{ name: "description", content:"Donate to the cause and help out our animals"}
   }
 });
-onMounted(()=>{
-let donateScript=document.createElement('script');
-donateScript.setAttribute('src', "https://donorbox.org/widget.js");
-document.head.appendChild(donateScript);
-});
-
 </script>

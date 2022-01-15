7b170268c66f43362a215563591f5d90bf0d5a8f
tyreln <tyrelnarciso@hotmail.com>
2022-01-15 14:53:39 -0800

    service worker removal patch

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
   <router-view />
 </template>
 <script>
-import { defineComponent, onBeforeMount } from 'vue';
-import { useStore } from 'vuex';
-import { api } from './boot/axios'
+import { defineComponent, onBeforeMount } from "vue";
+import { useStore } from "vuex";
+import { api } from "./boot/axios";
 export default defineComponent({
   preFetch({ store }) {
-  store.commit('initializeStore');
-  store.commit('setLoading', false);
-  const token = store.getters.token
-  api.defaults.headers.common['Authorization'] = token ? 'Token ' + token : "";
-},
-})
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+      console.log("service workers removed")
+    });
+    store.commit("initializeStore");
+    store.commit("setLoading", false);
+    const token = store.getters.token;
+    api.defaults.headers.common["Authorization"] = token
+      ? "Token " + token
+      : "";
+  },
+});
 </script>
